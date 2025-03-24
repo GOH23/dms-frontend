e6f2330b46d06d77f22ef5d0873da99767d4fa18
GOH23 <padadamlpd@gmail.com>
2025-03-24 23:42:24 +0300

    fix deploy

--- src/app/documents/[id]/page.tsx
+++ src/app/documents/[id]/page.tsx
@@ -3,17 +3,17 @@ import { securedApi } from "../../ui/api/axios";
 import { redirect } from "next/navigation";
 import { DocumentDetail } from "./DocumentDetail";
 
-export default async function DocumentDetailPage({ params }: { params: { id: string } }) {
-    const { id } = params;
+export default async function DocumentDetailPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     const cookie = await cookies();
-    
+
     try {
         // Получаем информацию о документе
         const document = await securedApi(cookie.get("token")?.value).get(`/documents/${id}`);
-        
+
         // Получаем историю документа
         const history = await securedApi(cookie.get("token")?.value).get(`/documents/${id}/history`).catch(() => ({ data: [] }));
-        
+
         return (
             <main className="container mx-auto">
                 <p className="text-xl text-center font-bold">Детали документа</p>
